1de8c33883aca56e2d625c6fbd21a17129877c38
totora0155 <sski0155@gmail.com>
2015-01-15 16:54:35 +0900

    add .css, .js

---
+++ app/script/script.js
@@ -0,0 +1,6 @@
+(function() {
+  console.log(123);
+
+}).call(this);
+
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbInNjcmlwdC5jb2ZmZWUiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IkFBQUE7QUFBQSxFQUFBLE9BQU8sQ0FBQyxHQUFSLENBQVksR0FBWixDQUFBLENBQUE7QUFBQSIsImZpbGUiOiJzY3JpcHQuanMiLCJzb3VyY2VSb290IjoiL3NvdXJjZS8iLCJzb3VyY2VzQ29udGVudCI6WyJjb25zb2xlLmxvZyAxMjNcbiJdfQ==
